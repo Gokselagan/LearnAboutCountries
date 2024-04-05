13492c4ac74a21f085c11e36ed311bb97a38a007
Gokselagan <gokselagan@outlook.com>
2024-04-05 03:41:07 +0300

    Unit_Test_Added_But_With_Errors

---
+++ src/modules/user/LogIn/Login.test.js
@@ -0,0 +1,45 @@
+
+import { render, screen } from "@testing-library/react"
+import { LogIn } from "."
+
+test("userFirstName input should be rendered", ()=>{
+    render(<LogIn/>);
+    const userInputEl = screen.getByLabelText(/first name/i);
+    expect(userInputEl).toBeInTheDocument();
+})
+
+test("userLastName input should be rendered", ()=>{
+    render(<LogIn/>);
+    const userInputEl = screen.getByLabelText(/last name/i);
+    expect(userInputEl).toBeInTheDocument();
+})
+
+test("userEmail input should be rendered", ()=>{
+    render(<LogIn/>);
+    const userInputEl = screen.getByPlaceholderText(/email/i);
+    expect(userInputEl).toBeInTheDocument();
+})
+
+test("userPassword input should be rendered", ()=>{
+    render(<LogIn/>);
+    const userInputEl = screen.getByPlaceholderText(/password/i);
+    expect(userInputEl).toBeInTheDocument();
+})
+
+test("remember control label should be rendered", ()=>{
+    render(<LogIn/>);
+    const userInputEl = screen.getByLabelText(/remember me/i);
+    expect(userInputEl).toBeInTheDocument();
+})
+
+test("button input should be rendered", ()=>{
+    render(<LogIn/>);
+    const userInputEl = screen.getByRole(/button/i);
+    expect(userInputEl).toBeInTheDocument();
+})
+
+test("forget password link input should be rendered", ()=>{
+    render(<LogIn/>);
+    const userInputEl = screen.getByRole("link", {name: /forgot password/i});
+    expect(userInputEl).toBeInTheDocument();
+})
